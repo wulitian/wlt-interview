9869f32a3e33ca45ba9bb89eb598b63b947c0183
wulitian <1334416530@qq.com>
2024-06-19 17:41:58 +0800

    修改文件内容并添加文档

--- 17几个重点/实现虚拟滚动/index.js
+++ 17几个重点/实现虚拟滚动/index.js
@@ -1,73 +1,81 @@
 class VirtualList {
-    constructor(containerSelection, listSelection) {
-        this.oContainer = document.querySelector(containerSelection);
-        this.oList = document.querySelector(listSelection);
-        this.startIndex = 0;
-        this.endIndex = 0;
-        this.renderList = [];
+    constructor(container, list) {
+        this.oContainer = document.querySelector(container)
+        this.oList = document.querySelector(list)
+        this.startIndex = 0
+        this.endIndex = 0
+        this.renderList = []
         this.scrollStyle = {}
         this.state = {
             dataSource: [],
             itemHeight: 100,
             viewHeight: 0,
-            maxCount: 0,
-        };
+            maxCount: 0
+        }
     }
 
-    init() {
+    computedViewHeight() {
         this.state.viewHeight = this.oContainer.offsetHeight;
+    }
+
+    computedMaxCount() {
         this.state.maxCount = Math.ceil(this.state.viewHeight / this.state.itemHeight) + 1;
-        this.addData();
-        this.render();
-        this.bindEvent();
+    }
+
+    computedStartIndex() {
+        const {scrollTop} = this.oContainer;
+        this.startIndex = Math.floor(scrollTop / this.state.itemHeight);
     }
 
     computedEndIndex() {
-        const end = this.startIndex + this.state.maxCount;
-        this.endIndex = this.state.dataSource[end] ? end : this.state.dataSource.length;
+        this.endIndex = this.startIndex + this.state.maxCount;
     }
 
     computedRenderList() {
-        const otherCount = 10;
-        let resEndIndex = this.endIndex + otherCount < this.state.dataSource.length ? this.endIndex + 10 : this.endIndex;
-        this.renderList = this.state.dataSource.slice(this.startIndex, resEndIndex);
+        this.renderList = this.state.dataSource.slice(this.startIndex, this.endIndex)
     }
 
     computedScrollStyle() {
         this.scrollStyle = {
-            height: `${
-                this.state.dataSource.length * this.state.itemHeight - this.state.itemHeight * this.startIndex
-            }px`,
-            transform: `translate3d(0, ${this.startIndex * this.state.itemHeight}px, 0)`
+            height: `${this.state.dataSource.length * this.state.itemHeight - this.startIndex * this.state.itemHeight}px`,
+            transform: `translateY(${this.startIndex * this.state.itemHeight}px)`
         }
     }
 
-    handleScroll() {
-        const {scrollTop} = this.oContainer;
-        this.startIndex = Math.floor(scrollTop / this.state.itemHeight);
-        this.render();
-    }
-
-    bindEvent() {
+    addEvent() {
         this.oContainer.addEventListener('scroll', this.handleScroll.bind(this))
     }
 
+    handleScroll() {
+        this.computedStartIndex();
+        this.render();
+    }
     addData() {
-        for (let i = 0; i < 10000; i++) {
-            const len = this.state.dataSource.length;
-            this.state.dataSource.push(len + 1)
+        for (let i = 0; i < 1000; i++) {
+            this.state.dataSource.push(i+1);
         }
     }
-
     render() {
         this.computedEndIndex();
         this.computedRenderList();
         this.computedScrollStyle();
-        const template = this.renderList.map(i => `<div class="virtual-list-item">${i}</div>`).join('')
+        const template = this.renderList.map(item=>{
+            return `<div class="virtual-list-item">${item}</div>`
+        }).join('');
         this.oList.innerHTML = template;
         this.oList.style.height = this.scrollStyle.height;
         this.oList.style.transform = this.scrollStyle.transform;
     }
+
+    init() {
+        this.computedViewHeight();
+        this.computedMaxCount();
+        this.addData();
+        this.render();
+        this.addEvent();
+    }
+
+
 }
 
 let virtualList = new VirtualList('.virtual-container', '.virtual-list');
